fa7c68e898896544d730525622833b57821ec306
Wellington Santos <wellington200@yahoo.com.br>
2021-10-21 22:33:07 -0300

    news2

---
+++ main.js
@@ -0,0 +1,34 @@
+const linksSocialMedia = {
+  github: 'Lordownerpy',
+  youtube: 'channel/Djfirewell',
+  facebook: 'wellington.santos.Viva',
+  instagram: 'Dj_firewelloficial',
+  twitter: 'nao existe'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+// #imginarConstruirTransformar
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
